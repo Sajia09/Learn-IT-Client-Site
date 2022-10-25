4d17c7db1ee2ce7e229c345968ad15df9bf33864
Sajia09 <sajiaa09@gmail.com>
2022-10-26 03:33:26 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            This is Header
+        <div className="navbar bg-blue-300">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">LearnIT</a>
+  </div>
+  <div className="flex-none text-xl font-semibold">
+  <ul className="menu menu-horizontal p-0">
+      <Link className='mr-3' to='/'>Home</Link>
+      <Link className='mr-3' to='/courses'>Courses</Link>
+      <Link className='mr-3' to='/blog'>Blog</Link>
+      <Link className='mr-3' to='/faq'>FAQ</Link>
+      <Link className='mr-3' to='/register'>Register</Link>
+      <Link className='mr-3' to='/login'>Login</Link>
+    </ul>
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" />
         </div>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
     );
 };
 
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,7 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Courses from "../../Pages/Courses/Courses";
+import FAQ from "../../Pages/FAQ/FAQ";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
 
 export const routes = createBrowserRouter([
     {
-        
+        path:'/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/courses',
+                element:<Courses></Courses>
+            },
+            {
+                path:'/blog',
+                element:<Blog></Blog>
+            },
+            {
+                path:'/faq',
+                element:<FAQ></FAQ>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            },
+            {
+                path:'/register',
+                element:<Register></Register>
+            },
+        ]
     }
 ])
