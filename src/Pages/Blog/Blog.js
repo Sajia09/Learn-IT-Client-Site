a593459656ef289748727018b6a6b406435cacf2
Sajia09 <sajiaa09@gmail.com>
2022-10-27 17:32:15 +0600

    blog added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 const Blog = () => {
     return (
         <div>
-            <h2>This is blog</h2>
+           <Container>
+           <h2>Question & Answers</h2>
+           <Row xs={1} md={2} className="g-4">
+        <Col>
+          <Card className='mb-5'>
+            <Card.Body>
+              <Card.Title>What is cors?</Card.Title>
+              <Card.Text>
+              Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>What is cors?</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className='mb-5'>
+            <Card.Body>
+              <Card.Title>What is cors?</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>What is cors?</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+    </Row>
+           </Container>
         </div>
     );
 };
